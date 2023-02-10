a3ba82d2f000fbc71568c0af2690d39c94f8012c
Jordan Sinclair <jordan.sinclair@du.edu>
2023-02-10 04:30:30 -0700

    Added comments

--- wall_flower/src/make_table.py
+++ wall_flower/src/make_table.py
@@ -9,37 +9,53 @@ from std_srvs.srv import Empty
 import json
 
 class makeTable():
-    q = {}
-    states = []
-    actions = {}
-    def __init__(self, states, actions):
+    q = {} #Queue table
+    states = [] #List of possible states
+    actions = {} #Dictionary of actions and the best option (all initialized to 0)
+    def __init__(self, states, actions): 
+        """
+        Class constructor. Sets the states and actions. 
+
+        Args:
+            states (List): List of possible states for this system
+            actions (Dictionary): Dictionary of actions and their corresponding utilities. 
+        """        
         self.states = states
         self.actions = actions
     
     def formatDict(self):
+        """
+        Formats the dictionary to contain all of the action values for a single state. 
+
+        Returns:
+            Dictionary: Formatted queue table
+        """        
         for state in self.states:
             self.q[state] = self.actions
         return self.q
     
     def saveToJson(self):
+        """
+        Generates a json file using the queue table determined above. 
+        """        
         with open("QQ.json", "w") as f:
             json.dump(self.q, f, indent=4)
     
-    def loadJson(self):
-        with open("QQ.json", "r") as f:
-            qDict = json.load(f)
-        return qDict
 
 
+#Sets up the actions and states I chose to use. 
 actions = {"forward": 0, "right": 0, "left": 0}
 states = []
 base_states = ["close", "medium", "far"]
+#Loops through the base state three times to create a string containing
+#each of forward, right, and left. 
 for one in base_states:
     for two in base_states:
         for three in base_states:
             state = f"forward: {one}, right: {two}, left: {three}"
             states.append(state)
 
+#Makes, formats, and saves the queue table. 
 tableMaker = makeTable(states, actions)
 tableMaker.formatDict()
 tableMaker.saveToJson()
--- wall_flower/src/move.py
+++ wall_flower/src/move.py
@@ -4,38 +4,57 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-import pandas as pd
 from std_srvs.srv import Empty
 import json
 import numpy as np
 
 class Move():
+    #Defines the possible action set and how they will be represented using twists. 
+    #The first entry gives the velocity in the x direction, and the second is the 
+    #angular velocity around z. 
     twists = {
-        "right": [0.25, 0.7],
-        "left": [0.25, -0.7],
-        "forward": [0.35, 0]
+        "right": [0.25, 0.95],
+        "left": [0.25, -0.95],
+        "forward": [0.3, 0]
     }
+
     def __init__(self):
+        """
+        Constructor initializing the node, services, publisher, and subscriber. 
+        """        
         self.init_node()
         self.init_services()
         self.init_publisher()
         self.init_subscriber()
     
     def init_services(self):
+        """
+        Generates a rospy service using gazebo's reset world. This creates a 
+        function to reset the world later using ServiceProxy. 
+        """        
         rospy.wait_for_service("/gazebo/reset_world")
         self.reset_world = rospy.ServiceProxy("/gazebo/reset_world", Empty)
     
     def init_node(self):
+        #Initializes a node for this project
         rospy.init_node("wallFlower")
     
     def init_publisher(self):
+        #Initializes a publisher to publish Twist messages on cmd_vel to move the robot. 
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
     
     def init_subscriber(self):
+        #Sets up a subscriber from rospy that consumers the Laserscan /scan topic. 
         sub = rospy.Subscriber("/scan", LaserScan, self.callback)
         rospy.spin()
     
     def publishTwist(self, twister):
+        """
+        Helper method to publish twists using the publisher defined above. 
+
+        Args:
+            twister (List): [linear velocity in x, angular velocity in z]
+        """        
         t = Twist()
         t.linear.x = twister[0]
         t.linear.y = 0
@@ -47,84 +66,87 @@ class Move():
 
     
     def splitRange(self, ranges):
-        front = [r for r in ranges[344:359] if r != "inf"] + [c for c in ranges[0:14] if c != "inf"]
+        """
+        This function takes a list of ranges (distance to an object) from the
+        robot and sorts and gets the minimum value for three quadrants of the
+        lidar sensor. 
+
+        Args:
+            ranges (List): List of range values based on 360 degrees of data. 
+        """        
+        front = [r for r in ranges[339:359] if r != "inf"] + [c for c in ranges[0:19] if c != "inf"]
         front = min(front)
         left = [i for i in ranges[44:134] if i != "inf"]
         left = min(left)
         right = [j for j in ranges[224:314] if j != "inf"]
         right = min(right)
-   #     print(str(front) + "\t" + str(left) + "\t" + str(right))
         return (front, left, right)
     
-    def getTable(self):
-        with open("Q.json", "r") as f:
+    def getTable(self, filename):
+        """
+        Opens the Q table that I defined (initially created in the make_table file)
+
+        Returns:
+            dictionary: Queue table in dictionary format
+        """        
+        with open(filename, "r") as f:
             q = json.load(f)
         return q
     
-    def getTwist(self, q):
-        newQ = {}
-        for key in q.keys():
-
-            if q[key]["right"] == 1:
-                t = Twist()
-                t.linear.x = 1
-                t.linear.y = 0
-                t.linear.z = 0
-                t.angular.x = 0
-                t.angular.x = 0
-                t.angular.z = 1
-                newQ[key] = t
-            elif q[key]["left"] == 1:
-                t = Twist()
-                t.linear.x = 1
-                t.linear.y = 0
-                t.linear.z = 0
-                t.angular.x = 0
-                t.angular.x = 0
-                t.angular.z = -1
-                newQ[key] = t
-
-#                print(newQ)
-        return newQ        
 
     def callback(self, dist):
+        """
+        Callback for the subscriber. Generates decisions, thresholds, states, and
+        publishes twists. 
+
+        Args:
+            dist (Dictionary): full set of data returned from the subscriber. 
+        """        
         dist.ranges = list(np.clip(dist.ranges, 0, 3.5))
+        #Resets the robot if it is really close to a wall. 0.12 meters is the minimum sensor
+        #range, so I thought it would be a reasonable value here. 
         for d in dist.ranges: 
-            if d < 0.2:
-                self.reset_world()
-    #    print("callback")
- #       rospy.loginfo(len(dist.ranges))
-   #     self.reset_world()
-        state = self.splitRange(dist.ranges)
+            if d < 0.12: 
+                self.reset_world() #resets the world
+
+        state = self.splitRange(dist.ranges) #Gets the state in terms of distance from the front, left, and right sides
         
+        #This section formats the input data as a string to compare it with the Q-Table. 
         res = ""
+        close_min = 0
+        close_max = 0.7
+        medium_min = 0.7
+        medium_max = 1.7
+        far_min = 1.7
+        far_max = 3.5
         #forward
-        if 0 <= state[0] <= 0.8:
+        if close_min <= state[0] <= close_max:
             res += "forward: close, "
-        if 0.8 <= state[0] <= 2.0:
+        if medium_min <= state[0] <= medium_max:
             res += "forward: medium, "
-        if 2.0 <= state[0] <= 3.5:
+        if far_min <= state[0] <= far_max:
             res += "forward: far, "
 
         #right
-        if 0 <= state[1] <= 0.8:
+        if close_min <= state[1] <= close_max:
             res += "right: close, "
-        if 0.8 <= state[1] <= 2.0:
+        if medium_min <= state[1] <= medium_max:
             res += "right: medium, "
-        if 2.0 <= state[1] <= 3.5:
+        if far_min <= state[1] <= far_max:
             res += "right: far, "
         
         #left
-        if 0 <= state[2] <= 0.8:
+        if close_min <= state[2] <= close_max:
             res += "left: close"
-        if 0.8 <= state[2] <= 2.0:
+        if medium_min <= state[2] <= medium_max:
             res += "left: medium"
-        if 2.0 <= state[2] <= 3.5:
+        if far_min <= state[2] <= far_max:
             res += "left: far"
         
-        q = self.getTable()
+        q = self.getTable("Q.json") #Gets the q table
+        #Calculates the maximum utility action as defined in the dictionary.
         val = max(q[res], key=q[res].get)
-        print(val)
+        #Publishes the twists to send the robot a move command. 
         self.publishTwist(self.twists[val])
 
 if __name__ == "__main__":
